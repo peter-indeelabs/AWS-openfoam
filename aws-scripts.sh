e9439adb4a2d5bd1b130b697d56fc1a549ab9887
peter-indeedlabs <56539381+peter-indeedlabs@users.noreply.github.com>
2019-10-14 03:11:29 -0700

    AWS scripts
    download and install openFOAM (Vesrion 6) on AWS

---
+++ aws-scripts.sh
@@ -0,0 +1,13 @@
+sudo apt-get -y update
+sudo apt-get -y upgrade
+sudo apt-get -y install apt-file
+sudo apt-get -y install software-properties-common
+sudo sh -c "wget -O - http://dl.openfoam.org/gpg.key | apt-key add -"
+sudo add-apt-repository http://dl.openfoam.org/ubuntu
+sudo apt-get -y install wget
+sudo apt-get -y install apt-transport-https
+sudo apt-get -y update
+sudo apt-get -y install openfoam6
+sudo echo "source /opt/openfoam6/etc/bashrc" >> ~/.bashrc
+sudo apt install unzip
+sudo apt install awscli
