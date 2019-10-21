a06d786b59a56869d98f7af34e3db47deab668c5
peter-indeelabs <56539381+peter-indeelabs@users.noreply.github.com>
2019-10-21 12:07:38 -0700

    Update aws-scripts.sh

--- aws-scripts.sh
+++ aws-scripts.sh
@@ -2,9 +2,9 @@ sudo apt-get -y update
 sudo apt-get -y upgrade
 sudo apt-get -y install apt-file
 sudo apt-get -y install software-properties-common
+sudo apt-get -y install wget
 sudo sh -c "wget -O - http://dl.openfoam.org/gpg.key | apt-key add -"
 sudo add-apt-repository http://dl.openfoam.org/ubuntu
-sudo apt-get -y install wget
 sudo apt-get -y install apt-transport-https
 sudo apt-get -y update
 sudo apt-get -y install openfoam6
