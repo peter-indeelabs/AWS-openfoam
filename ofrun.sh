9ba2a2effbb734748d38f7d5d8edd0ee6029d8f2
peter-indeelabs <56539381+peter-indeelabs@users.noreply.github.com>
2019-11-04 23:27:06 -0800

    Update ofrun.sh

--- ofrun.sh
+++ ofrun.sh
@@ -4,7 +4,7 @@ source ~/.bashrc
 export PATH=~/.local/bin:$PATH
 date
 echo "Args: $@"
-#env
+env
 echo "Welcome to AWS OpenFOAM built by Indee."
 echo "jobId: $AWS_BATCH_JOB_ID"
 echo "jobQueue: $AWS_BATCH_JQ_NAME"
@@ -13,9 +13,12 @@ sudo mkdir /fsx
 sudo mount -t lustre -o noatime,flock fs-03f7dae7c222360f2.fsx.us-west-2.amazonaws.com@tcp:/fsx /fsx
 echo "fs-03f7dae7c222360f2.fsx.us-west-2.amazonaws.com@tcp:/fsx /fsx lustre defaults, noatime,flock,_netdev 0 0" >> /etc/fstab 
 cat /etc/fstab
-cd /fsx
+mkdir /fsx/run-sim18-2
+cd /fsx/run-sim18-2
 echo $PWD
-df
+su - fong
 aws s3 sync s3://indeenfsworkdir/sim18-run-batch .
-
+ls -ltr
 decomposePar
+mpirun -np 50 pimpleFoam -parallel
+reconstructPar -time "0.0034:0.003415"
